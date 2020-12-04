6ad5ae172cd650b41169efda8abe96f8e9dfed01
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-04 16:04:32 -0600

    ...

--- appimpiv2/cassandraSent.py
+++ appimpiv2/cassandraSent.py
@@ -4,7 +4,7 @@ from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import SimpleStatement
 import os
 
-timeOut=100
+timeOut=1000
 cloud_config= {
         'secure_connect_bundle': '/app/appimpiv2/secure-connect-dbquart.zip'
     }
