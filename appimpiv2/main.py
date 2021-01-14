267d8c418abb7604d6b4b53a2a5d857700072a2c
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-14 08:49:57 -0600

    ...

--- appimpiv2/main.py
+++ appimpiv2/main.py
@@ -59,10 +59,9 @@ if status==200:
             print('Restarting sequential NO FOUND counter to Zero')
             query="update thesis.cjf_control set noinfolimit=0 where id_control="+str(objControl.idControl)+"; "
             bd.executeNonQuery(query)
-            query='update thesis.cjf_control set page='+str(num)+' where id_control=10'
+            query='update thesis.cjf_control set page='+str(num+1)+' where id_control=10'
             bd.executeNonQuery(query)
-            for x in range(1,3):
-                browser.back()  
+            
         else:
             print('Zero rows in the search...bye bye')
             #Look and update nolimit count
