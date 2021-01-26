50df8611e8a8a69ec2112ae502c62d7f94c1f2d2
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-26 13:42:36 -0600

    ...

--- appimpiv2_2019/InternalControl.py
+++ appimpiv2_2019/InternalControl.py
@@ -2,10 +2,10 @@ import os
 
 class cInternalControl:
     idControl=11
-    version='migration'
+    version='2019'
     timeout=70
     hfolder='appimpiv2_'+version 
-    heroku=True
+    heroku=False
     rutaHeroku='/app/'+hfolder
     rutaLocal=os.getcwd()+'\\'+hfolder+'\\'
     download_dir='Download_'+hfolder
