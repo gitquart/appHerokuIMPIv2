9c347aab48d048207dc3696b3a2ea10ecab3f9a4
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-06 14:43:31 -0500

    ...

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 /app/appimpiv2_2020B/main.py
+worker: python3 /app/appimpiv2_2015/main.py
--- appimpiv2_2020B/cassandraSent.py
+++ appimpiv2_2020B/cassandraSent.py
@@ -16,10 +16,19 @@ def getCluster():
     #Connect to Cassandra
     objCC=CassandraConnection()
     cloud_config=''
+    zip_file='secure-connect-dbquart_serverless.zip'
     if objControl.heroku:
-        cloud_config= {'secure_connect_bundle': objControl.rutaHeroku+'/secure-connect-dbquart.zip'}
+        cloud_config= {'secure_connect_bundle': objControl.rutaHeroku+'/'+zip_file,
+                       'init-query-timeout': 10,
+                       'connect_timeout': 10,
+                       'set-keyspace-timeout': 10
+        }
     else:
-        cloud_config= {'secure_connect_bundle': objControl.rutaLocal+'secure-connect-dbquart.zip'}
+        cloud_config= {'secure_connect_bundle': objControl.rutaLocal+zip_file,
+                       'init-query-timeout': 10,
+                       'connect_timeout': 10,
+                       'set-keyspace-timeout': 10
+        }
 
 
     auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
@@ -103,8 +112,8 @@ def getFieldsFromTable(keyspace,table):
 
    
 class CassandraConnection():
-    cc_user='quartadmin'
+    cc_user='psXzplCMoTnTjWLSeAblXSkr'
     cc_keyspace='thesis'
-    cc_pwd='P@ssw0rd33'
+    cc_pwd='_vGwtwEUsOxj9ZuSbm6SOJhyQOsU-s9QEzX7ZWZcRYKeew9sOqPeFmKr-pDzsIoisiF8aInS1HjSs7_fqZ9EZaMS96TNCwn_yReyjZvHuys+_fGSBuHKZz_+NTj6Z6L0'
         
 
